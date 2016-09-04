de56565fb99139b0d172071bd367cfec9fa86b52
Christos Karolemeas <ckarol@christoss-mbp>
2016-09-04 16:29:44 +0300

    Initial Commit

---
+++ Queue/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  Queue
+//
+//  Created by Christos Karolemeas on 04/09/16.
+//  Copyright © 2016 Christos Karolemeas. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
